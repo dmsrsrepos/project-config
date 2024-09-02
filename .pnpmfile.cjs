96c51d513df089295be6b510dea86bba253ebc2f
Jimbo <happycoder@foxmail.com>
2024-09-02 16:18:24 +0800

    配置和依赖更新： - 在VSCode设置中启用pnpm目录镜像。 - 重构扩展的配置属性以支持新的文件嵌套更新器配置。 - 在package.json中更新多个依赖项，包括typescript和vscode-ext-gen。 - 调整pnpm-lock.yaml以反映依赖项和校验和的变化。
    此次提交主要涉及开发配置和依赖更新，为项目引入了新功能和潜在的稳定性改进。

---
+++ .pnpmfile.cjs
@@ -0,0 +1,41 @@
+const fs = require('node:fs')
+const devPackagePaths = {
+    "vscode-ext-gen": [
+        `C:\\AppData\\code-vsextension\\vscode-ext-gen`
+    ]
+}
+module.exports = {
+    hooks: {
+        readPackage: (/*** @type {PackageJson} */ pkg, context) => {
+
+            Object.entries(devPackagePaths).forEach(([name, paths]) => {
+                paths.forEach(path => {
+                    if (fs.existsSync(path)) {
+                        const locationProtocol = `file:${path}`
+                        if (pkg.dependencies?.[name]) {
+                            pkg.dependencies[name] = locationProtocol
+                            context.log(`package '${name}' of dependencies redirect to ${locationProtocol}`)
+                        }
+                        if (pkg.devDependencies?.[name]) {
+                            pkg.devDependencies[name] = locationProtocol
+                            context.log(`package '${name}' of devDependencies redirect to ${locationProtocol}`)
+                        }
+                        if (pkg.peerDependencies?.[name]) {
+                            pkg.peerDependencies[name] = locationProtocol
+                            context.log(`package '${name}' of peerDependencies redirect to ${locationProtocol}`)
+                        }
+                        if (pkg.optionalDependencies?.[name]) {
+                            pkg.optionalDependencies[name] = locationProtocol
+                            context.log(`package '${name}' of optionalDependencies redirect to ${locationProtocol}`)
+                        }
+                        if (pkg.bundleDependencies?.[name]) {
+                            pkg.bundleDependencies[name] = locationProtocol
+                            context.log(`package '${name}' of bundleDependencies redirect to ${locationProtocol}`)
+                        }
+                    }
+                });
+            });
+            return pkg;
+        }
+    }
+};
