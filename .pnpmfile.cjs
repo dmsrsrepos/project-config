5d938dd0c5d7bb4b87ed4f7348c02229a2bca22f
cnjimbo <cnjimbo@dmsrs.org>
2024-10-17 01:18:57 +0800

    更新构建配置和依赖项
    - 修改 .pnpmfile.cjs 以支持调试模式
    - 更新 package.json 中 reactive-meta-gen 的版本
    - 更新 pnpm-lock.yaml 中的依赖项版本
    - 修改 tsconfig.json，添加 typeRoots 和排除 node_modules

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,5 +1,5 @@
 const fs = require('node:fs')
-
+const isdebug = false
 const devPackagePaths = {
   'reactive-meta-gen': [
     `C:\\AppData\\code-vsextension\\reactive-meta-gen`,
@@ -9,33 +9,35 @@ const devPackagePaths = {
 module.exports = {
   hooks: {
     readPackage: (/** * @type {PackageJson} */ pkg, context) => {
-      Object.entries(devPackagePaths).forEach(([name, paths]) => {
-        paths.forEach((path) => {
-          if (fs.existsSync(path)) {
-            const locationProtocol = `file:${path}`
-            if (pkg.dependencies?.[name]) {
-              pkg.dependencies[name] = locationProtocol
-              context.log(`package '${name}' of dependencies redirect to ${locationProtocol}`)
+      if (isdebug) {
+        Object.entries(devPackagePaths).forEach(([name, paths]) => {
+          paths.forEach((path) => {
+            if (fs.existsSync(path)) {
+              const locationProtocol = `file:${path}`
+              if (pkg.dependencies?.[name]) {
+                pkg.dependencies[name] = locationProtocol
+                context.log(`package '${name}' of dependencies redirect to ${locationProtocol}`)
+              }
+              if (pkg.devDependencies?.[name]) {
+                pkg.devDependencies[name] = locationProtocol
+                context.log(`package '${name}' of devDependencies redirect to ${locationProtocol}`)
+              }
+              if (pkg.peerDependencies?.[name]) {
+                pkg.peerDependencies[name] = locationProtocol
+                context.log(`package '${name}' of peerDependencies redirect to ${locationProtocol}`)
+              }
+              if (pkg.optionalDependencies?.[name]) {
+                pkg.optionalDependencies[name] = locationProtocol
+                context.log(`package '${name}' of optionalDependencies redirect to ${locationProtocol}`)
+              }
+              if (pkg.bundleDependencies?.[name]) {
+                pkg.bundleDependencies[name] = locationProtocol
+                context.log(`package '${name}' of bundleDependencies redirect to ${locationProtocol}`)
+              }
             }
-            if (pkg.devDependencies?.[name]) {
-              pkg.devDependencies[name] = locationProtocol
-              context.log(`package '${name}' of devDependencies redirect to ${locationProtocol}`)
-            }
-            if (pkg.peerDependencies?.[name]) {
-              pkg.peerDependencies[name] = locationProtocol
-              context.log(`package '${name}' of peerDependencies redirect to ${locationProtocol}`)
-            }
-            if (pkg.optionalDependencies?.[name]) {
-              pkg.optionalDependencies[name] = locationProtocol
-              context.log(`package '${name}' of optionalDependencies redirect to ${locationProtocol}`)
-            }
-            if (pkg.bundleDependencies?.[name]) {
-              pkg.bundleDependencies[name] = locationProtocol
-              context.log(`package '${name}' of bundleDependencies redirect to ${locationProtocol}`)
-            }
-          }
+          })
         })
-      })
+      }
       return pkg
     },
   },
